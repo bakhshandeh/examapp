9b2d8c427e00a7c8c9429d097518ba01aeef106d
Morteza Dastouri <dastouri70@gmail.com>
2013-09-27 16:54:38 +0330

    fixed mine.js

--- js/mine.js
+++ js/mine.js
@@ -68,11 +68,13 @@ $(function() {
 		    //$('#ef-portfolio').isotope('reLayout');
 		    var $container = $('#ef-portfolio');
                     var $newdiv = $("<div class='ef-col1-4 ef-item'> <div class='proj-img clickable'> <img src=uploads/" + filename + " alt=''> <div class='proj-description'> <h4> <a href='#'>" + title.val() + "</a> </h4> <p>" + context.val() + "</p> <div class='ef-proj-links'> <div class='alignright'> <a class='ef-proj-more'> </a> </div> </div> </div> </div> </div> ");
-                    $('#addBlock').click(function(){
+                    $container.isotope('insert',$newdiv);
+		    /*
+		    $('#addBlock').click(function(){
 			    $container.isotope('insert',$newdiv);
 			    return false;
 			  });
-		    
+		    */
 		   /* var $container = $('#ef-portfolio');
 		    $container.isotope('reLayout');*/
 		    
