3dd1feca962dea81ab6361352124e79d734eb795
Morteza Dastouri <dastouri70@gmail.com>
2013-09-27 17:38:01 +0330

    fixed uploading

--- uploadifive/uploadifive.php
+++ uploadifive/uploadifive.php
@@ -5,7 +5,7 @@ Copyright (c) 2012 Reactive Apps, Ronnie Garcia
 */
 
 // Set the uplaod directory
-$uploadDir = '/uploadifive/uploads/';
+$uploadDir = '/examapp/uploads/';
 
 // Set the allowed file extensions
 $fileTypes = array('jpg', 'jpeg', 'gif', 'png'); // Allowed file extensions
