5dba4bf6d37d935e83d2940e084bf8948ad1dfd3
cna <cna@cna-G60JX.(none)>
2013-09-27 17:21:22 +0330

    hover fixed

--- js/custom2.js
+++ js/custom2.js
@@ -1,3 +1,4 @@
+$(document).ready(function() {
 $(function(){
     
   
@@ -9,3 +10,31 @@ $container.imagesLoaded( function(){
   });
 });
 
+/*Hovers*/
+	
+    $('.proj-img').has('.ef-proj-more').hover(function(){
+
+        $(this).find('.proj-description').stop().animate({
+            "opacity": "1"
+        }, 400).children(':first-child').stop().animate({
+        	top: '0'
+        }, 200).next().stop().animate({
+        	top: '0'
+        }, 220).next().stop().animate({
+        	bottom: '0'
+        }, 200);
+        
+    }, function() {
+
+        $(this).find('.proj-description').stop().animate({
+            "opacity": "0"
+        }, 400).children(':first-child').stop().animate({
+        	top: '-40px'
+        }, 200).next().stop().animate({
+        	top: '-50px'
+        }, 220).next().stop().animate({
+        	bottom: '-75px'
+        }, 200);
+        
+    });
+});
